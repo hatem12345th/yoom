5e375f6e4994f0bcd77a5b27fb0c1d1862d2a29b
hatem12345th <h.taleb@esi-sba.dz>
2024-08-02 23:27:27 +0100

    Navbar finish

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image"
+import Link from "next/link"
+import { MobileNav } from "./MobileNav"
 
 
 export const Navbar = () => {
   return (
-    <div></div>
+    <nav className="flex-between f fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+          <Link href={"/"} className="items-center flex-between gap-2" >
+          <Image 
+            src="/icons/logo.svg"
+            width={45}
+            height={45}
+              alt="yoom logo"
+          className="max-sm:size-10"
+          />
+ <p className="text-[26px] flex-between font-extrabold text-white max-sm:hidden">
+          YOOM
+        </p>
+      
+          </Link>
+      <div className=" flex-between   gap-5">
+        {/*    Clerk User Management */}
+        <MobileNav />
+
+      </div>
+
+          
+    </nav>
   )
 }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -14,14 +14,14 @@ export const Sidebar = () => {
     flex flex-col justify-between p-6 pt-28 max-sm:hidden
         left-0 top-0
     '>
-        <div className='flex flex-col gap-6'>
+        <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((link) => {
-              const isActive = pathname === link.route || (pathname.startsWith(link.route) && pathname === "/")
+              const isActive = pathname === link.route || (pathname.startsWith(`${link.route}/`) )
             return (
               <Link 
               href={link.route} 
               key={link.label}
-              className={cn('flex gap-4 items-center p-4  rounded-lg justify-start',{
+              className={cn('flex gap-4 items-center p-4 hover:bg-blue rounded-lg justify-start',{
                 'bg-blue':isActive,
               })}
               >
